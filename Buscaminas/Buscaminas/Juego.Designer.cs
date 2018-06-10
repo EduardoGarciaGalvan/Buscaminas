639372dfffacfb59e927b75160371589aa527b69
david <idv16a.arodriguez@uartesdigitales.edu.mx>
2018-06-10 17:08:57 -0500

    Add resources

--- Buscaminas/Buscaminas/Juego.Designer.cs
+++ Buscaminas/Buscaminas/Juego.Designer.cs
@@ -28,109 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(25, 37);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(85, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(8, 18);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(16, 26);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(24, 34);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(32, 42);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
             // 
             // Juego
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 303);
-            this.Controls.Add(this.groupBox1);
             this.MaximumSize = new System.Drawing.Size(600, 650);
             this.MinimumSize = new System.Drawing.Size(300, 350);
             this.Name = "Juego";
             this.Text = "Juego";
             this.Load += new System.EventHandler(this.Juego_Load);
-            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
     }
 }
--- Buscaminas/Buscaminas/Juego.cs
+++ Buscaminas/Buscaminas/Juego.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Buscaminas.Properties;
 
 namespace Buscaminas
 {
@@ -19,12 +20,55 @@ namespace Buscaminas
 
         private void Juego_Load(object sender, EventArgs e)
         {
+            int x, y, mines;
+            x = y = 9;
+            mines = 10;
 
         }
 
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        private class TileGrid:Panel
         {
+            private Size gridSize;
+            private int mines;
+            private int flags;
+
+            internal void loadGrid (Size gridSize, int mines)
+            {
+                this.Controls.Clear();
+                this.gridSize = gridSize;
+                this.mines = this.flags = mines;
+                this.Size = new Size(gridSize.Width * Tile.LENGTH, gridSize.Height * Tile.LENGTH);
+                for (int c = 0; c < gridSize.Width; c++)
+                {
+                    for (int z = 0; z < gridSize.Height; z++)
+                    {
+                        Tile cell = new Tile(); 
+                    }
+                }
+            }
+
+            private class Tile:PictureBox
+            {
+                internal const int LENGTH = 25;
+
+                internal Point GridPosition
+                {
+                    get;
+                }
+
+                internal Tile(int x, int y)
+                {
+                    // It is comes into its own when used to build strings with reference to other values. What previously had to be written as:
+                    this.Name = $"Tile_{x}_{y}";
+                    this.Location = new Point(x * LENGTH, y * LENGTH);
+                    this.GridPosition = new Point(x, y);
+                    this.Size = new Size(LENGTH, LENGTH);
+                    this.Image = Resources.Tile; 
+                }
+            }
 
         }
+
+
     }
 }
